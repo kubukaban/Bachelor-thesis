637b5fd26827f86a9b2d8a4571d155725b4e7a86
kubukaban <152863411+kubukaban@users.noreply.github.com>
2024-05-22 21:37:22 +0200

    Add files via upload

--- testovanie.r
+++ testovanie.r
@@ -1,37 +1,43 @@
-data1 <- read.csv("bunnyMiddle.csv")
-data2 <- read.csv("bunnyComplex.csv")
+data1 <- read.csv("csv/bunnyMiddle.csv")
+data2 <- read.csv("csv/bunnyComplex.csv")
 
-# data are not from normal distribution*( shoking I know)
-ks.test(data1$pomer_polomerov, "pnorm", mean = mean(data1$pomer_polomerov), sd = sqrt(var(data1$pomer_polomerov))) # p-value < 2.2e-16 not normal
-ks.test(data2$pomer_polomerov, "pnorm", mean = mean(data2$pomer_polomerov), sd = sqrt(var(data2$pomer_polomerov))) #  p-value < 2.2e-16 not normal
-hist(data1$pomer_polomerov)
-hist(data2$pomer_polomerov)
-qqnorm(data1$pomer_polomerov, pch = 1, frame = FALSE)# vyzera ze je posunuty
-qqline(data1$pomer_polomerov, col = "steelblue", lwd = 2)
-qqnorm(data2$pomer_polomerov, pch = 1, frame = FALSE)# vyzera ze je posunuty
-qqline(data2$pomer_polomerov, col = "steelblue", lwd = 2)
+# data are not from normal distribution*( schoking I know)
+#rho
+ks.test(data1$Radius.ratio, "pnorm", mean = mean(data1$Radius.ratio), sd = sqrt(var(data1$Radius.ratio))) # p-value < 2.2e-16 not normal
+ks.test(data2$Radius.ratio, "pnorm", mean = mean(data2$Radius.ratio), sd = sqrt(var(data2$Radius.ratio))) #  p-value < 2.2e-16 not normal
+#tau
+ks.test(data1$Edge.ratio, "pnorm", mean = mean(data1$Edge.ratio), sd = sqrt(var(data1$Edge.ratio))) # p-value < 2.2e-16 not normal
+ks.test(data2$Edge.ratio, "pnorm", mean = mean(data2$Edge.ratio), sd = sqrt(var(data2$Edge.ratio))) #  p-value < 2.2e-16 not normal
+#nu
+ks.test(data1$Circumradius.to.half.perimeter, "pnorm", mean = mean(data1$Circumradius.to.half.perimeter), sd = sqrt(var(data1$Circumradius.to.half.perimeter))) # p-value < 2.2e-16 not normal
+ks.test(data2$Circumradius.to.half.perimeter, "pnorm", mean = mean(data2$Circumradius.to.half.perimeter), sd = sqrt(var(data2$Circumradius.to.half.perimeter))) #  p-value < 2.2e-16 not normal
+#iota
+ks.test(data1$Aspect.ratio, "pnorm", mean = mean(data1$Aspect.ratio), sd = sqrt(var(data1$Aspect.ratio))) # p-value < 2.2e-16 not normal
+ks.test(data2$Aspect.ratio, "pnorm", mean = mean(data2$Aspect.ratio), sd = sqrt(var(data2$Aspect.ratio))) #  p-value < 2.2e-16 not normal
+#omega
+ks.test(data1$Circumradius.to.edge, "pnorm", mean = mean(data1$Circumradius.to.edge), sd = sqrt(var(data1$Circumradius.to.edge))) # p-value < 2.2e-16 not normal
+ks.test(data2$Circumradius.to.edge, "pnorm", mean = mean(data2$Circumradius.to.edge), sd = sqrt(var(data2$Circumradius.to.edge))) #  p-value < 2.2e-16 not normal
 
+hist(data1$Radius.ratio)
+hist(data2$Radius.ratio)
 
-#
-ks.test(data1$pomer_polomerov, "pnorm", mean = mean(data1$pomer_polomerov), sd = sqrt(var(data1$pomer_polomerov))) # p-value < 2.2e-16 not normal
 
 # h0 complex = middle h1 compplex != middle
-wilcox.test(data1$pomer_polomerov,data2$pomer_polomerov, alternative = "two.sided")
+wilcox.test(data1$Radius.ratio,data2$Radius.ratio, alternative = "two.sided")
 #p-value < 2.2e-16 # h0 rejected
 
 # h0 middle >= complex h1 midlle <= complex
-wilcox.test(data1$pomer_polomerov,data2$pomer_polomerov, alternative = "less")
+wilcox.test(data1$Radius.ratio,data2$Radius.ratio, alternative = "less")
 #  p-value = 1 h0 not rejected
 
-wilcox.test(data1$pomer_extrem_stran,data2$pomer_extrem_stran, alternative = "two.sided")
+wilcox.test(data1$Edge.ratio,data2$Edge.ratio, alternative = "two.sided")
 # p-value = 0.3704 h0 not rejected
 
-wilcox.test(data1$pomer_polomer_polobvod,data2$pomer_extrem_stran, alternative = "two.sided")
-# p-value < 2.2e-16 h0 rejected
-wilcox.test(data1$pomer_stran,data2$pomer_stran, alternative = "two.sided")
-# p-value < 2.2e-16 h0 rejected
-wilcox.test(data1$pomer_opisana_strana,data2$pomer_opisana_strana, alternative = "two.sided")
+wilcox.test(data1$Circumradius.to.half.perimeter,data2$Circumradius.to.half.perimeter, alternative = "two.sided")
 # p-value < 2.2e-16 h0 rejected
 
-cor(data1$obsahy,data1$min_uhol)
+wilcox.test(data1$Aspect.ratio,data2$Aspect.ratio, alternative = "two.sided")
+# p-value < 2.2e-16 h0 rejected
+wilcox.test(data1$Circumradius.to.edge,data2$Circumradius.to.edge, alternative = "two.sided")
+# p-value < 2.2e-16 h0 rejected
 
